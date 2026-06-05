4b9bfd8603f506734586a82ba3599e00a40da421
Gokul Sivaji <gokulsivaji@gokulsivaji.local>
2026-06-05 03:58:47 -0700

    improved json formatting

--- app/arb.py
+++ app/arb.py
@@ -1,25 +1,36 @@
 from .api import getData
+
 TAX_RATE = 0.0125
 
-#main method, holds the math behind the arb
 def analyze():
     data = getData()
-    product = data.get("products", {})
+    products = data.get("products", {})
 
     items = {}
-    for product in data["products"]:
-        sellPrice = data["products"][product]["quick_status"]["sellPrice"]
-        buyPrice = data["products"][product]["quick_status"]["buyPrice"]
-        buyVolume = data["products"][product]["quick_status"]["buyMovingWeek"]
-        sellVolume = data["products"][product]["quick_status"]["sellMovingWeek"]
-        profit = (1-TAX_RATE) * sellPrice - buyPrice
 
-        if (buyVolume + sellVolume) == 0:
+    for product_id, product_data in products.items():
+        status = product_data.get("quick_status", {})
+
+        sellPrice = status.get("sellPrice", 0)
+        buyPrice = status.get("buyPrice", 0)
+        buyVolume = status.get("buyMovingWeek", 0)
+        sellVolume = status.get("sellMovingWeek", 0)
+
+        profit = (1 - TAX_RATE) * sellPrice - buyPrice
+
+        if buyVolume + sellVolume == 0:
             popularityScore = 0
         else:
             popularityScore = (2 * buyVolume * sellVolume) / (buyVolume + sellVolume)
-        if(profit > 1000 and popularityScore > 1000):
-            name = " ".join(product.lower().split("_")) 
-            items[name] = int(round(profit, 0))
+
+        if profit > 1000 and popularityScore > 1000:
+            name = " ".join(product_id.lower().split("_"))
+
+            items[name] = {
+                "profit": int(round(profit, 0)),
+                "buyPrice": buyPrice,
+                "sellPrice": sellPrice,
+                "popularityScore": int(popularityScore),
+            }
 
     return items
